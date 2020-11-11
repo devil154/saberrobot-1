ac486941b786583f9de07524dc0567ddaecb3091
•/ ＭＲ ∆ＮOＮ <51384033+noobanon@users.noreply.github.com>
2020-11-11 16:45:43 +0530

    Update alternate.py

--- tg_bot/modules/helper_funcs/alternate.py
+++ tg_bot/modules/helper_funcs/alternate.py
@@ -1,32 +1,25 @@
-import sys
-import traceback
-
 from functools import wraps
-from typing import Optional
-
+from telegram import error, ChatAction
 from telegram import User, Chat, ChatMember, Update, Bot
 from telegram import error
 
-from tg_bot import DEL_CMDS, SUDO_USERS, WHITELIST_USERS
-
-
-def send_message(message, text,  *args,**kwargs):
-	try:
-		return message.reply_text(text, *args,**kwargs)
-	except error.BadRequest as err:
-		if str(err) == "Reply message not found":
-			return message.reply_text(text, quote=False, *args,**kwargs)
+def send_message(message, text, *args, **kwargs):
+    try:
+        return message.reply_text(text, *args, **kwargs)
+    except error.BadRequest as err:
+        if str(err) == "Reply message not found":
+            return message.reply_text(text, quote=False, *args, **kwargs)
 
 
 def typing_action(func):
     """Sends typing action while processing func command."""
 
     @wraps(func)
-    def command_func(update, context, *args, **kwargs):
-        context.bot.send_chat_action(
+    def command_func(bot: Bot, update: Update, *args, **kwargs):
+        bot.send_chat_action(
             chat_id=update.effective_chat.id, action=ChatAction.TYPING
         )
-        return func(update, context, *args, **kwargs)
+        return func(bot: Bot, update: Update, *args, **kwargs)
 
     return command_func
 
@@ -36,11 +29,11 @@ def send_action(action):
 
     def decorator(func):
         @wraps(func)
-        def command_func(update, context, *args, **kwargs):
-            context.bot.send_chat_action(
+        def command_func(bot: Bot, update: Update, *args, **kwargs):
+            bot.send_chat_action(
                 chat_id=update.effective_chat.id, action=action
             )
-            return func(update, context, *args, **kwargs)
+            return func(bot: Bot, update: Update, *args, **kwargs)
 
         return command_func
 
